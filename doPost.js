0235d8791b0019e118044c7b625ad2492a06da77
naka4o <naka4o.hd@gmail.com>
2025-11-27 20:49:13 +0900

    Update doPost.js

--- doPost.js
+++ doPost.js
@@ -6,7 +6,8 @@ async function doPost() {
 <h3>データ送信中.. 数秒お待ちください ..</h3>
 `;
 // decrypto
-    const URL='https://script.google.com/macros/s/AKfycbyn3kjIYtpNr6IYXhV_04GY9e6gsTOiM090MAM1WyN9Oq--uKTJoRrcPULlTeOBEghD5A/exec';
+    //const URL='https://script.google.com/macros/s/AKfycbyn3kjIYtpNr6IYXhV_04GY9e6gsTOiM090MAM1WyN9Oq--uKTJoRrcPULlTeOBEghD5A/exec';
+    const URL='https://script.google.com/macros/s/AKfycbzLVksrx6uWaQsKNWyTV_as3vBmJ3FhisoGJCUv7bL_LpFPYZZyQOm02rbVMlaV173A9g/exec';
     //const param='U2FsdGVkX1%2ByVECD2yA0%2FFUPWcGfxBevTiLbQIQvgZWtXRdbgDM0%2Ft%2BmhPKyOrtO';
     try {
       /*
